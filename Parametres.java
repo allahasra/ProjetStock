23775f4fcb7f9907280609b9de0ee7e8f8e21467
allahasra <33481199+allahasra@users.noreply.github.com>
2022-01-17 12:32:27 +0000

    Create Parametres.java

---
+++ Parametres.java
@@ -0,0 +1,9 @@
+package basedonnees;
+public class Parametres {
+    public static String IPHOST="127.0.0.0 ";//adresse de bloucage
+    public static String HOST_DB="jdbc:mysql://"+IPHOST+":3306/magasin";
+    public static String USERNAME_DB="root";
+    public static String PASSWORD_DB="root";
+    public static int PORT=111111;
+    public static String USER;
+}
